1f5f764f5d84c7d8fc99cb36ad1a8f0ed7581bd5
Fabián Valencia <fbvalencia@gmail.com>
2026-09-10 19:39:19 -0600

    Add retryOrder and resolveManually functions

--- src/preload.js
+++ src/preload.js
@@ -7,5 +7,7 @@ contextBridge.exposeInMainWorld('pos', {
   syncQueueNow: () => ipcRenderer.invoke('queue:sync-now'),
   getQueueSummary: () => ipcRenderer.invoke('queue:summary'),
   getQueueErrors: () => ipcRenderer.invoke('queue:errors'),
+  retryOrder: (orderId) => ipcRenderer.invoke('queue:retry-order', orderId),
+  resolveManually: (orderId, note) => ipcRenderer.invoke('queue:resolve-manually', { orderId, note }),
   onQueueUpdated: (callback) => ipcRenderer.on('queue:updated', (_e, data) => callback(data)),
 });
