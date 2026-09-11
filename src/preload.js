d35874714ac1621b7afc24f78340e6ea0ed2f17a
Fabián Valencia <fbvalencia@gmail.com>
2026-09-11 00:51:38 -0600

    Add cashInfo parameter to checkout function

--- src/preload.js
+++ src/preload.js
@@ -6,7 +6,7 @@ contextBridge.exposeInMainWorld('pos', {
   toFileUrl: (localPath) => (localPath ? pathToFileURL(localPath).href : null),
   syncCatalogNow: () => ipcRenderer.invoke('catalog:sync-now'),
   getProducts: (search) => ipcRenderer.invoke('catalog:get-products', { search }),
-  checkout: (cartItems, paymentMethod) => ipcRenderer.invoke('order:checkout', { cartItems, paymentMethod }),
+  checkout: (cartItems, paymentMethod, cashInfo) => ipcRenderer.invoke('order:checkout', { cartItems, paymentMethod, cashInfo }),
   syncQueueNow: () => ipcRenderer.invoke('queue:sync-now'),
   getQueueSummary: () => ipcRenderer.invoke('queue:summary'),
   getQueueErrors: () => ipcRenderer.invoke('queue:errors'),
